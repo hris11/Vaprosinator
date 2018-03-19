3f4c491287fae5b3ab64565c96c2eeb27be5b7b7
Slavkata <slavkirilov00@gmail.com>
2018-03-19 14:42:23 +0200

    Achievement service methods

--- src/main/java/hristian/nikola/slav/services/AchievementService.java
+++ src/main/java/hristian/nikola/slav/services/AchievementService.java
@@ -1,4 +1,32 @@
 package hristian.nikola.slav.services;
 
+import hristian.nikola.slav.models.Achievement;
+
+import java.util.List;
+
 public class AchievementService {
+    public List<Achievement> getAchievements() {
+        return null;
+    }
+
+    public List<Achievement> getPlayerAchievemnts(int playerId) {
+        return null;
+    }
+
+    public Achievement completeAchievement(int playerId, int achievementId) {
+        return null;
+    }
+
+    public Achievement createAchievements(int playerId) {
+        //add incomplete achievements to a player
+        return null;
+    }
+
+    public List<Achievement> getPlayerCompleteAchievements(int playerId) {
+        return null;
+    }
+
+    public List<Achievement> getPlyaerIncompleteAchievements(int playerId) {
+        return null;
+    }
 }
