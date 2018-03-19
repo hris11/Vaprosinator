9ace25f5093c92b53f72c93553ad4ad1376a73bb
Slavkata <slavkirilov00@gmail.com>
2018-03-19 14:47:10 +0200

    Answer service methods

--- src/main/java/hristian/nikola/slav/services/AnswerService.java
+++ src/main/java/hristian/nikola/slav/services/AnswerService.java
@@ -1,4 +1,26 @@
 package hristian.nikola.slav.services;
 
+import hristian.nikola.slav.models.Answer;
+
+import java.util.List;
+
 public class AnswerService {
+    public Answer getCorrectAnswer(int questionId){
+        return null;
+    }
+
+    public List<Answer> getQuestionAnswers(int questionId) {
+        return null;
+    }
+
+    public List<Answer> createAnswers(List<Answer> answers, int questionId) {
+        //set all as incorrect
+        return null;
+    }
+
+    public  Answer setAnswerAsCorrect(int questionId, int answerId) {
+        return null;
+    }
+
+    
 }
