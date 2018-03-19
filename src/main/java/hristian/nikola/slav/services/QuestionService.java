31cc3f99a70cee9ffd155c8b2cddcfa420bbcae5
Slavkata <slavkirilov00@gmail.com>
2018-03-19 15:00:46 +0200

    Question Service methods

--- src/main/java/hristian/nikola/slav/services/QuestionService.java
+++ src/main/java/hristian/nikola/slav/services/QuestionService.java
@@ -1,4 +1,30 @@
 package hristian.nikola.slav.services;
 
+import hristian.nikola.slav.models.Question;
+
+import java.util.List;
+
 public class QuestionService {
+    private List<Question> questionList;
+
+    public QuestionService() {
+        questionList = getQuestions();
+    }
+
+    private List<Question> getQuestions() {
+        return null;
+    }
+
+    public Question getQuestion(int questionId) {
+        return null;
+    }
+
+    public Question addQuestion(Question question) {
+        return null;
+    }
+
+    public List<Question> updateQuestionList() {
+        //add more questions to the question list
+        return null;
+    }
 }
