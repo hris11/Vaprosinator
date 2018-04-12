fb6d427995de08c9b9fe1b02892cbe01c44f0f3e
hris11 <hris11@abv.bg>
2018-04-12 10:44:01 +0300

    Register and login components

--- vaprosinator-angular/src/app/app.module.ts
+++ vaprosinator-angular/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthHandlerComponent } from './auth-handler/auth-handler.component';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
+import { AuthRegisterComponent } from './auth-register/auth-register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthHandlerComponent
+    AuthHandlerComponent,
+    AuthLoginComponent,
+    AuthRegisterComponent
   ],
   imports: [
     BrowserModule
