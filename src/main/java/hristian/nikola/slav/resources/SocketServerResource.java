3dca54771092a0e1d9aca69da06004a4203e8965
ivaylo99 <ivaylovasilev99@gmail.com>
2018-04-30 20:32:48 +0300

    Untested socket server + gradle dep.

--- src/main/java/hristian/nikola/slav/resources/SocketServerResource.java
+++ src/main/java/hristian/nikola/slav/resources/SocketServerResource.java
@@ -1,35 +1,50 @@
 package hristian.nikola.slav.resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import java.io.BufferedReader;
+import sun.plugin2.message.Message;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
-@Path("socket")
+@ServerEndpoint("/socket")
 public class SocketServerResource {
-    private int port = 6350;
-    private ServerSocket serverSocket;
-    private Socket serviceSocket;
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
 
-    @GET
-    public void startSocket() {
-        try {
-            serverSocket = new ServerSocket(port, 1 , InetAddress.getLocalHost());
-            serviceSocket = serverSocket.accept();
-            System.out.println(serviceSocket.getInetAddress().getHostAddress());
-            PrintWriter out = new PrintWriter(serviceSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(serviceSocket.getInputStream()));
-            String data;
-            while ( (data = in.readLine()) != null ) {
-                System.out.println("\r\nMessage from : " + data);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    @OnMessage
+    public void onMessage(Session session, String message) throws IOException {
+        if (message != null) {
+            broadcast(message);
         }
+    }
+
+    @OnOpen
+    public void onOpen(Session peer) throws IOException {
+        peers.add(peer);
+    }
+
+    @OnClose
+    public void onClose(Session peer) throws IOException {
+        peers.remove(peer);
+    }
+
+    private static void broadcast(String message)
+            throws IOException {
+
+        peers.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.getBasicRemote().sendText(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
